a3423c7a4fc1a572cfea5ec1321b2a623b29ab76
hyunito <eujengonzales@gmail.com>
2025-10-26 13:37:51 +0800

    Major changes: Added Reserved Word, Logical, Single and Multi-line Comments. Test cases has been fixed

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int main() {
-    FILE *fp = fopen("../test/test_basic.pyclang", "r");
+    FILE *fp = fopen("../test/test_error.pyclang", "r");
     if (!fp) {
         printf("Error: could not open file.\n");
         return 1;
@@ -11,11 +11,11 @@ int main() {
 
     char c;
     int state = 0;
-    char buffer[100];
+    char buffer[999];
     int idx = 0;
 
     while ((c = fgetc(fp)) != EOF) {
-        switch (state) {
+        switch(state) {
             case 0:
                 if (isalpha(c)) { 
                     buffer[idx++] = c;
@@ -24,13 +24,31 @@ int main() {
                     buffer[idx++] = c;
                     state = 2;
                 } else if (c == '=') {
-                    printf("ASSIGN_OP(=)\n");
+                    int next = fgetc(fp);
+                    if (next == '=') {
+                        printf("REL_OP(==)\n");
+                    } else {
+                        printf("ASSIGN_OP(=)\n");
+                        ungetc(next, fp);
+                    }
+                } else if (c == '#') {
+                    printf("COMMENT(#)\n");
+                    state = 4;
+                } else if (c == '/'){
+                    int next = fgetc(fp);
+                    if (next == '*') {
+                        printf("COMMENT_START(/*)\n");
+                        state = 5;
+                    } else {
+                        printf("DIV(/)\n");
+                        ungetc(next, fp);
+                    }
                 } else if (c == ';') {
                     printf("SEMICOLON(;)\n");
                 } else if (c == '(') {
-                    printf("LEFT_PAREN\n");
+                    printf("LEFT_PAREN(()\n");
                 } else if (c == ')') {
-                    printf("RIGHT_PAREN\n");
+                    printf("RIGHT_PAREN())\n");
                 } else if (c == '<') {
                     int next = fgetc(fp);
                     if (next == '=') {
@@ -47,14 +65,6 @@ int main() {
                         printf("REL_OP(>)\n");
                         ungetc(next, fp);
                     }
-                } else if (c == '-') {
-                    int next = fgetc(fp);
-                    if (next == '-') {
-                        printf("DECREMENT\n");
-                    } else {
-                        printf("ARITH_OP(-)\n");
-                        ungetc(next, fp);
-                    }
                 } else if (c == '{') {
                     printf("LEFT_BRACE({)\n");
                 } else if (c == '}') {
@@ -68,17 +78,25 @@ int main() {
                 } else if (c == '+') {
                     int next = fgetc(fp);
                     if (next == '+') {
-                        printf("INCREMENT\n");
+                        printf("INCREMENT(++)\n");
                     } else {
-                        printf("ARITH_OP(+)\n");
+                        printf("ADD(+)\n");
+                        ungetc(next, fp);
+                    }
+                } else if (c == '-') {
+                    int next = fgetc(fp);
+                    if (next == '-') {
+                        printf("DECREMENT(--)\n");
+                    } else {
+                        printf("SUB(-)\n");
                         ungetc(next, fp);
                     }
                 } else if (isspace(c)) {
-                   
+
                 } else if (c == '"') {
                     state = 3; 
                 } else {
-                    printf("UNKNOWN(%c)\n", c);
+                    printf("ERROR(%c)\n", c);
                 }
                 break;
 
@@ -87,9 +105,13 @@ int main() {
                     buffer[idx++] = c;
                 } else {
                     buffer[idx] = '\0';
-                    if (strcmp(buffer, "int") == 0 || strcmp(buffer, "float") == 0 || strcmp(buffer, "output") == 0 || strcmp(buffer, "bool") == 0 || strcmp(buffer, "True") == 0 || strcmp(buffer, "if") == 0 || strcmp(buffer, "else") == 0 || strcmp(buffer, "while") == 0 || strcmp(buffer, "for") == 0)
+                    if (strcmp(buffer, "output") == 0 || strcmp(buffer, "input") == 0 || strcmp(buffer, "if") == 0 || strcmp(buffer, "else") == 0 || strcmp(buffer, "while") == 0 || strcmp(buffer, "for") == 0 || strcmp(buffer, "break") == 0 || strcmp(buffer, "continue") == 0 || strcmp(buffer, "return") == 0) {
                         printf("KEYWORD(%s)\n", buffer);
-                    else
+                    } else if (strcmp(buffer, "int") == 0 || strcmp(buffer, "float") == 0 || strcmp(buffer, "bool") == 0 || strcmp(buffer, "char") == 0 || strcmp(buffer, "str") == 0 || strcmp(buffer, "void") == 0 || strcmp(buffer, "True") == 0 || strcmp(buffer, "False") == 0) {
+                        printf("RESERVED_WORD(%s)\n", buffer);
+                    } else if (strcmp(buffer, "and") == 0 || strcmp(buffer, "or") == 0 || strcmp(buffer, "not") == 0) {
+                        printf("LOGICAL(%s)\n", buffer);
+                    } else
                         printf("IDENTIFIER(%s)\n", buffer);
                     idx = 0;
                     state = 0;
@@ -97,7 +119,7 @@ int main() {
                 }
                 break;
 
-            case 2: 
+            case 2:
                 if (isdigit(c)) {
                     buffer[idx++] = c;
                 } else if (c == '.') {
@@ -119,6 +141,36 @@ int main() {
                     printf("STRING(%s)\n", buffer);
                     idx = 0;
                     state = 0;
+                }
+                break;
+
+            case 4:
+                if (c != '\n') {
+                    buffer[idx++] = c;
+                } else {
+                    buffer[idx] = '\0';
+                    printf("COM_STR(%s)\n", buffer);
+                    idx = 0;
+                    state = 0;
+                }
+                break;
+
+            case 5:
+                if (c == '*') {
+                    int next = fgetc(fp);
+                    if (next == '/') {
+                        buffer[idx] = '\0';
+                        printf("COM_STR(%s)\n", buffer);
+                        printf("COMMENT_END(*/)\n");
+                        idx = 0;
+                        state = 0;
+                    } else {
+                        buffer[idx++] = c;
+                        ungetc(next, fp);
+                        break;
+                    }
+                } else {
+                    buffer[idx++] = c;
                 }
                 break;
         }
